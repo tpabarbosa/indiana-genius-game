571d65d499917b9070492676ca0e3e8f97d89128
Tatiana Barbosa <tpabarbosa@yahoo.com.br>
2022-04-07 18:27:42 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,161 @@
+const startBtn = document.getElementById("start-button");
+const splashScreen = document.getElementById("splash-screen");
+const main = document.getElementById("main");
+const menu = document.getElementById("menu");
+const gameArea = document.getElementById("game");
+const gameStatus = document.getElementById("game-status");
+const gameMessage = document.getElementById("status-message");
+const gameScore = document.getElementById("game-score");
+const normalModeBtn = document.getElementById("normal-mode");
+const restartGameBtn = document.getElementById("restart-game");
+const btn4 = document.getElementById("btn-4");
+const btn5 = document.getElementById("btn-5");
+const btn6 = document.getElementById("btn-6");
+const btn7 = document.getElementById("btn-7");
+
+startBtn.onclick = () => {
+    splashScreen.classList.toggle("hidden");
+    main.classList.toggle("hidden");
+    setTimeout(() => {
+        playMelody();
+    }, 800);
+};
+
+normalModeBtn.onclick = () => {
+    gameArea.classList.toggle("hidden-right");
+    menu.classList.toggle("hidden-left");
+    music.isPlaying = false;
+    gameScore.innerText = `Fase ${game.score}`;
+    setTimeout(gameLoop, 1200);
+};
+
+restartGameBtn.onclick = () => {
+    newGame();
+};
+
+btn4.onclick = () => {
+    playPlayerNote(0);
+};
+btn5.onclick = () => {
+    playPlayerNote(1);
+};
+btn6.onclick = () => {
+    playPlayerNote(2);
+};
+btn7.onclick = () => {
+    playPlayerNote(3);
+};
+
+const TURN = { computer: 0, player: 1 };
+
+const game = {
+    turn: TURN.computer,
+    computerSequence: [],
+    playerSequence: [],
+    ended: false,
+    score: 0,
+    frequencies: [332.6, 444, 166.32, 279.6],
+    buttons: [btn4, btn5, btn6, btn7],
+    playInterval: 300,
+    playingNotes: [],
+};
+
+const gameLoop = () => {
+    if (!game.ended) {
+        game.playerSequence = [];
+        gameScore.innerText = `Fase ${game.score}`;
+        const color = Math.floor(Math.random() * 4);
+        game.computerSequence.push(color);
+        game.playingNotes = [...game.computerSequence];
+        playComputerSequence();
+    }
+};
+
+const playComputerSequence = () => {
+    if (game.playingNotes.length === 0) {
+        changeToPlayerTurn();
+    }
+    setTimeout(() => {
+        if (game.playingNotes.length > 0) {
+            note = game.playingNotes.shift();
+            playNote(
+                game.frequencies[note],
+                game.playInterval,
+                game.buttons[note],
+                playComputerSequence
+            );
+        }
+    }, 600);
+};
+
+const changeToPlayerTurn = () => {
+    game.turn = TURN.player;
+};
+
+const playPlayerNote = (note) => {
+    if (game.turn !== TURN.player) {
+        return;
+    }
+
+    game.playerSequence.push(note);
+    playNote(game.frequencies[note], game.playInterval, game.buttons[note]);
+    checkSequence();
+};
+
+const checkSequence = () => {
+    for (let i in game.playerSequence) {
+        if (game.computerSequence[i] != game.playerSequence[i]) {
+            gameOver();
+            break;
+        }
+    }
+    if (game.playerSequence.length === 2 && !game.ended) {
+        victory();
+    }
+    if (
+        game.computerSequence.length === game.playerSequence.length &&
+        !game.ended
+    ) {
+        nextTurn();
+    }
+};
+
+const nextTurn = () => {
+    game.score++;
+    game.turn = TURN.computer;
+    setTimeout(gameLoop, 500);
+};
+
+const gameOver = () => {
+    game.ended = true;
+    gameMessage.innerHTML = `Você errou!! <br>Game Over`;
+    gameStatus.style.backgroundImage = "url(images/804591.jpg)";
+    gameStatus.style.filter = "brightness(2)";
+    setTimeout(() => {
+        gameStatus.classList.toggle("hidden");
+        gameStatus.classList.toggle("opacity-hidden");
+    }, 500);
+};
+
+const victory = () => {
+    game.ended = true;
+    gameMessage.innerHTML = `Vitória!! <br>Você acertou toda a sequência`;
+    gameStatus.style.backgroundImage = "url(images/804588.png)";
+    gameStatus.style.filter = "brightness(1)";
+    setTimeout(() => {
+        gameStatus.classList.toggle("hidden");
+        gameStatus.classList.toggle("opacity-hidden");
+    }, 500);
+};
+
+const newGame = () => {
+    game.turn = TURN.computer;
+    game.computerSequence = [];
+    game.playerSequence = [];
+    game.ended = false;
+    game.score = 0;
+    game.playingNotes = [];
+    gameStatus.classList.toggle("hidden");
+    gameStatus.classList.toggle("opacity-hidden");
+    gameLoop();
+};
---
+++ js/music.js
@@ -0,0 +1,113 @@
+// create web audio api context
+const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+
+const music = {
+    isPlaying: true,
+};
+
+const btn0 = document.getElementById("btn-0");
+const btn1 = document.getElementById("btn-1");
+const btn2 = document.getElementById("btn-2");
+const btn3 = document.getElementById("btn-3");
+
+const tempo = 384;
+
+function playNote(frequency, duration, btn, cb = null) {
+    // create Oscillator node
+    const oscillator = audioCtx.createOscillator();
+    const volume = audioCtx.createGain();
+    oscillator.connect(volume);
+    oscillator.type = "triangle";
+    oscillator.frequency.value = frequency; // value in hertz
+    volume.gain.value = 0.1;
+    volume.connect(audioCtx.destination);
+    oscillator.start();
+    btn.classList.add("shine");
+
+    setTimeout(function() {
+        oscillator.stop();
+        btn.classList.remove("shine");
+        if (cb) {
+            cb();
+        }
+    }, duration);
+}
+
+function playMelody() {
+    if (!music.isPlaying) {
+        return;
+    }
+    if (notes.length === 0) {
+        notes = [...melody];
+    }
+    if (notes.length > 0) {
+        // note = notes.pop();
+        note = notes.shift();
+        const time = note[2] * tempo;
+        playNote(note[0], time, note[1], playMelody);
+    }
+}
+
+const C = [264, btn0];
+const D = [296.2, btn1];
+const E = [332.6, btn2];
+const F = [352.4, btn3];
+const G = [395.5, btn0];
+const A = [444, btn1];
+const B = [498.4, btn2];
+const C2 = [528, btn3];
+const D2 = [592.42, btn0];
+const E2 = [665.28, btn1];
+const F2 = [704.88, btn2];
+
+const melody = [
+    [...E, 0.75],
+    [...F, 0.25],
+    //
+    [...G, 1],
+    [...C2, 2],
+    [...D, 0.75],
+    [...E, 0.25],
+    //
+    [...F, 3],
+    [...G, 0.75],
+    [...A, 0.25],
+    //
+    [...B, 1],
+    [...F2, 2],
+    [...G, 0.75],
+    [...A, 0.25],
+    //
+    [...B, 1],
+    [...C2, 1],
+    [...D2, 1],
+    [...E, 0.75],
+    [...F, 0.25],
+    //
+    [...G, 1],
+    [...C2, 2],
+    [...D2, 0.75],
+    [...E2, 0.25],
+    //
+    [...F2, 3],
+    [...G, 0.75],
+    [...G, 0.25],
+    //
+    [...E2, 1],
+    [...D2, 0.75],
+    [...G, 0.25],
+    [...E2, 1],
+    [...D2, 0.75],
+    [...G, 0.25],
+    //
+    [...E2, 1],
+    [...D2, 0.75],
+    [...G, 0.25],
+    [...F2, 1],
+    [...E2, 0.75],
+    [...D2, 0.25],
+    //
+    [...C2, 2],
+];
+
+let notes = [...melody];
